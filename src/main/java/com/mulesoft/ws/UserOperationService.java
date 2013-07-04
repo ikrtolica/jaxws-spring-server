2be1f87a40bd5a492071fdc042eedcd089112180
Igor Krtolica <igor@blinkinc.com>
2013-07-03 18:10:22 -0700

    Support for ActiveMQ Maven runs added.

--- src/main/java/com/mulesoft/ws/UserOperationService.java
+++ src/main/java/com/mulesoft/ws/UserOperationService.java
@@ -5,13 +5,38 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+
+import com.mulesoft.jms.SimpleMessageProducer;
+import javax.jms.JMSException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @WebService()
 @SOAPBinding(style = SOAPBinding.Style.RPC, use = SOAPBinding.Use.LITERAL)
 public class UserOperationService {
+    private static final Logger LOG = LoggerFactory.getLogger(UserOperationService.class);
     private static final String HELLO = "Hello";
 
     @WebMethod(operationName = "sayHello")
     public String sayHelloToTheUser(@WebParam(name = "name") String userName) {
+        ApplicationContext context
+                = new ClassPathXmlApplicationContext("./MessageContext.xml");
+        SimpleMessageProducer producer = (SimpleMessageProducer) context.getBean("messageProducer");
+
+        try {
+            producer.sendMessages(userName);
+        } catch (JMSException e) {
+            System.out.println("Could not send a message.");
+        }
+
+        LOG.info("Returning a HELLO message.");
+
         return HELLO + " " + userName;
     }
 }
